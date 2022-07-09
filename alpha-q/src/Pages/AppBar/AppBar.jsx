2fb06fbd5cf2ed8ff8d3045c8dae1dc9544e6e11
KlamLoh <klamloh@gmail.com>
2022-07-09 18:31:22 +0800

    changed appbar

--- alpha-q/src/Pages/AppBar/AppBar.jsx
+++ alpha-q/src/Pages/AppBar/AppBar.jsx
@@ -1,14 +1,28 @@
 import {
   AppBar as MuiAppBar,
   Box,
+  Button,
   IconButton,
+  Stack,
   Toolbar,
+  Typography,
 } from "@mui/material";
 import AppTitle from "./AppTitle";
 import MenuIcon from "@mui/icons-material/Menu";
+import { useNavigate } from "react-router-dom";
 
 
 export default function AppBar() {
+  const navigate = useNavigate();
+  const gotohome = () => {
+    navigate("/")
+  }
+  const gotologin = () => {
+    navigate("/login");
+  };
+  const gotosignup = () => {
+    navigate("/signup");
+  };
   return (
     <MuiAppBar
       position="fixed"
@@ -16,11 +30,18 @@ export default function AppBar() {
       sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
     >
       <Toolbar>
-        <IconButton color="primary" edge="start">
-          <MenuIcon />
-        </IconButton>
-        <AppTitle/>
-        <Box sx={{ flex: 1 }} />
+        <Button variant="contained" onClick={gotohome}>
+          {"Alpha Q"}
+        </Button>
+          <Box sx={{ flex: 1 }} />
+        <Stack direction="row" spacing={0} display="flex" flex="1" justifyContent="right" alignItems="center">
+          <Button variant="contained" onClick={gotologin}>
+          {"Login"}
+          </Button>
+          <Button variant="contained" onClick={gotosignup}>
+            {"Sign Up"}
+          </Button>
+        </Stack>
       </Toolbar>
     </MuiAppBar>
   );
